7802bfcab6a1e6da6f864c8d0a2ea2ab6851c243
Payev65 <pajew65@gmail.com>
2025-03-14 14:08:34 +0100

    Made a player class and adet movment to it

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     #____Game Loop____
     while True:
@@ -11,8 +21,16 @@ def main():
             if event.type == pygame.QUIT:
                 return
     
+        updatable.update(dt)
+
         screen.fill("black")
+        
+        for i in drawable:
+            i.draw(screen)
+        
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
